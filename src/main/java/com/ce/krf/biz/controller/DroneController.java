f63a0a7ccc64bf256248e06c86834c6d3e2d38ed
CHUL-PC\CHUL <pchul16@gmail.com>
2017-07-18 15:04:36 +0900

    DRONE-getRWMDT

--- src/main/java/com/ce/krf/biz/controller/DroneController.java
+++ src/main/java/com/ce/krf/biz/controller/DroneController.java
@@ -1,30 +1,43 @@
 package com.ce.krf.biz.controller;
 
-import java.util.HashMap;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ce.krf.KrfBizApplication;
 import com.ce.krf.biz.base.BaseController;
+import com.ce.krf.biz.model.ResultVO;
 import com.ce.krf.biz.service.DroneService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/drone")
 public class DroneController extends BaseController {
 	private final Logger logger = LoggerFactory.getLogger(KrfBizApplication.class);
-	
+
 	@Autowired
 	public DroneService droneService;
-	
-	@RequestMapping("/getRWMDT")
-	public HashMap<String, Object> getRWMDT(@RequestParam String siteCodes, @RequestParam String measureDate, String layerDate) {
+
+	@RequestMapping(value = "/getRWMDT", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+	public String getRWMDT(@RequestParam String siteCodes, @RequestParam String measureDate, String layerDate) {
+		ObjectMapper om = new ObjectMapper();
 		
-//		return droneService.getRWMDT(siteCodes, measureDate, layerDate);
-		return null;
+		ResultVO result = new ResultVO();
+		result.setDatas(droneService.getRWMDT(siteCodes, measureDate, layerDate));
+		try {
+			return om.writeValueAsString(result);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return "error";
+		}
 	}
 }
