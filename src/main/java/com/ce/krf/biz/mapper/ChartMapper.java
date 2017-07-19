c2d093dee8857122ae74fef90d8671e6816d23ce
hyeokpark92 <hyeokpark92@gmail.com>
2017-07-19 18:04:41 +0900

    H.P - GetRWMDT_C, GetRWMDT_D001 ~ GetRWMDT_D004

--- src/main/java/com/ce/krf/biz/mapper/ChartMapper.java
+++ src/main/java/com/ce/krf/biz/mapper/ChartMapper.java
@@ -7,6 +7,13 @@ import com.ce.krf.biz.model.ChartVO;
 
 public interface ChartMapper {
 	public List<Map<String, Object>> getRWMDT(String siteCd);
+	
 	public List getRWMDTSelectA(ChartVO param);
 	public List getRWMDTSelectB(ChartVO param);
+	public List getRWMDTSelectC(ChartVO param);
+	
+	public List getRWMDTSelectD001(ChartVO param);
+	public List getRWMDTSelectD002(ChartVO param);
+	public List getRWMDTSelectD003(ChartVO param);
+	public List getRWMDTSelectD004(ChartVO param);
 }
