2909fe214531d5add5073d253d97d79e6944a215
CHUL-PC\CHUL <pchul16@gmail.com>
2017-07-21 13:31:05 +0900

    # WARNING: head commit changed in the meantime
    AS-IS 의 파라메터와 리턴값에 맞게 수정

--- src/main/java/com/ce/krf/biz/controller/ChartController.java
+++ src/main/java/com/ce/krf/biz/controller/ChartController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ce.krf.KrfBizApplication;
@@ -30,9 +29,17 @@ public class ChartController extends BaseController{
 	public ChartService chartService;
 	
 	
-	@GetMapping(value = "/getRWMDT", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, Object> getRWMDT(@RequestParam String siteCd) {
-		return chartService.getRWMDT(siteCd);
+	@RequestMapping(value = "/getRWMDT", produces = "text/html; charset=euc-kr")
+	public String getRWMDT(@ModelAttribute ChartVO param) {
+		HashMap result = new HashMap();
+		try {
+			result.put("data", chartService.getRWMDT(param.getRecordId()));
+			return getEuckrString(result);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return "error";
+		}
 	}
 	
 	
