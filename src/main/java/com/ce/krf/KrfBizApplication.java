c3fac2d8dc6030630d5188c63650930c81070b8c
CHUL-PC\CHUL <pchul16@gmail.com>
2017-07-28 14:32:17 +0900

    서버 시작시 SPRING BOOT logo삭제

--- src/main/java/com/ce/krf/KrfBizApplication.java
+++ src/main/java/com/ce/krf/KrfBizApplication.java
@@ -1,16 +1,11 @@
 package com.ce.krf;
 
-import java.nio.charset.Charset;
-
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
  
 @SpringBootApplication
 @MapperScan("com.ce.krf.biz.mapper")
@@ -41,6 +36,8 @@ public class KrfBizApplication extends SpringBootServletInitializer {
 //        };
 //    }
 	public static void main(String[] args) {
-		SpringApplication.run(KrfBizApplication.class, args);
+		SpringApplication app = new SpringApplication(KrfBizApplication.class);
+	    app.setBannerMode(Banner.Mode.OFF);
+	    app.run(args);
 	}
 }
